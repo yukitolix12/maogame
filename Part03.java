05cb3f35b71dadc37762ed85c1e2b31e2facf8cf
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-08-16 10:16:06 +0900

    文章実験

--- Part03.java
+++ Part03.java
@@ -2,6 +2,11 @@ public class Part03
 {
     public static void main(String[] args)
     {
-        System.out.println("こんにちは");
+        System.out.println("魔王が世界を滅ぼそうとしている");
+        System.out.println("あなたはレベル99のツワモノです");
+        System.out.println("うわっ、あなたのレベル、高すぎ？");
+        System.out.println("あなたは魔王を倒しました");
+        System.out.println("レベル99なので余裕でしたwww");
+        System.out.println("GAME CLEAR!");
     }
 }
