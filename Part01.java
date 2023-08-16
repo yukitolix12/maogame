ba93af6a51dc1adf58c384a29e0dbcb9580cd364
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-08-16 22:19:06 +0900

    if文の完成

--- Part01.java
+++ Part01.java
@@ -3,15 +3,27 @@ public class Part01      //おまじない
     public static void main(String[] args)   //おまじない
     {
         String name = "すけさん";             //プレイヤーの名前
-        int lv = 40;                        //レベル
+        int lv = 45;                        //レベル
         System.out.println("魔王が" + "が世界を滅ぼそうとしている");
         System.out.println(name + "はレベル" + lv + "のツワモノです");
-        System.out.println(name + "は魔王を倒しました！");
-        if( lv > 50 ){
-            System.out.println("レベル" + lv + "なので倒せました");
+        
+        if( lv < 40 ){
+            System.out.println( name + "は魔王に敗れましたorz");
+            System.out.println( "GAME OVER");
         }else{
+            System.out.println(name + "は魔王を倒しました！");
+            if( lv > 200 ){
+            System.out.println("レベル" + lv + "なので魔王ゴミでしたwwww");
+            }else if( lv > 120 ){
+            System.out.println("レベル" + lv + "なので魔王弱すぎでしたwww");
+            }else if( lv > 80 ){
+            System.out.println("レベル" + lv + "なので余裕でした");
+            }else if( lv > 50 ){
+            System.out.println("レベル" + lv + "なので倒せました");
+            }else{
             System.out.println("レベル" + lv + "なので苦戦しました");
+            }
+            System.out.println( "GAME CLEAR!");
         }
-        System.out.println( "GAME CLEAR!");
     }
 }
