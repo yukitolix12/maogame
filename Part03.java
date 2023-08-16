0141ed1d4589fe573a238954d88b832048c7d0af
yukitolix12 <132648775+yukitolix12@users.noreply.github.com>
2023-08-16 10:21:24 +0900

    変数指定

--- Part03.java
+++ Part03.java
@@ -2,10 +2,11 @@ public class Part03
 {
     public static void main(String[] args)
     {
+        String name = "アーサー";
         System.out.println("魔王が世界を滅ぼそうとしている");
-        System.out.println("あなたはレベル99のツワモノです");
-        System.out.println("うわっ、あなたのレベル、高すぎ？");
-        System.out.println("あなたは魔王を倒しました");
+        System.out.println(name + "はレベル99のツワモノです");
+        System.out.println("うわっ、" + name + "のレベル、高すぎ？");
+        System.out.println(name + "は魔王を倒しました");
         System.out.println("レベル99なので余裕でしたwww");
         System.out.println("GAME CLEAR!");
     }
